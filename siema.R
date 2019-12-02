ab926c6e4e0c422136e2c71c308074f00e44393d
student <iguessmj@gmail.com>
2019-12-02 09:14:59 +0100

    Wprowadzenie danych

--- siema.R
+++ siema.R
@@ -1,2 +1,6 @@
 print("Siemano kolano")
 print("Elo pomelo")
+dane <- read.csv(file = "D:/STUDENCI/m.jankowska/Pi/Projekt/Ankieta.csv",
+                 header = T, encoding = "UTF-8")
+dane <- dane[,-1]
+
