2ced51e9465597c1e43ed76dd09860d6243ad71f
KMS131C4\wojci <w.rosa@pollub.pl>
2019-12-18 00:28:53 +0100

    To do..

---
+++ DO_ZROBIENIA.R
@@ -0,0 +1,7 @@
+# 2019-12-17 WR
+# W zdaniach:
+#P-value dla testu rho-Spearmana wynosi 0.0535368656, natomiast dla testu tau-Kendalla wynosi
+#0.0534696670. 
+# proszę dodać wartość współczynnika korelacji r = ...
+# Proszę o usunięcie tabel z liczebnościami
+# Proszę o dodanie i sformatowanie tabel z procentami
