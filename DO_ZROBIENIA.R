a559ba4640faa9225624fce2f2b788f70df8c76d
KMS131C4\wojci <w.rosa@pollub.pl>
2020-01-13 09:48:26 +0100

    Do zrobienia

--- DO_ZROBIENIA.R
+++ DO_ZROBIENIA.R
@@ -5,3 +5,11 @@
 # proszę dodać wartość współczynnika korelacji r = ...
 # Proszę o usunięcie tabel z liczebnościami
 # Proszę o dodanie i sformatowanie tabel z procentami
+# 2020-01-13 WR
+# Techniczne:
+# Proszę dodać imiona i nazwiska w tytule
+# Polskie znaki
+# oryginalne p = 0.0535368656 to wyświetlane p = 0.054.
+# oryginalne p = 0.535368656 to wyświetlane p = 0.535
+# oryginalne p = 0.00123 to wyświetlane p = 0.001**
+# oryginalne p = 0.000000535368656 to wyświetlane p < 0.001***
